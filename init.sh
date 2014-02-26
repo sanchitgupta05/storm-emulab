6abf62318d48134977e13b18daafb5b14d690aa4
Hilfi M. Alkaff <alkaff2@illinois.edu>
2014-02-25 21:25:57 -0700

    Change in syntax from supervisord 2.0 to 3.0.

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 # This script setups storm's dependencies, needs to be run in all hosts.
 
 MASTER_NODE="hosts-1.storm.uiucnet.emulab.net"
+REPO_DIR="/proj/uiucnet/exp/storm/storm-emulab/"
 
 sudo apt-get update
 sudo apt-get install -y openjdk-7-jre openjdk-7-jdk build-essential pkg-config libtool zookeeper zookeeper-bin chkconfig supervisor maven2
@@ -13,19 +14,21 @@ sudo make install
 cd /proj/uiucnet/exp/storm/deps/jzmq/
 sudo make install
 
-sudo chown -R halkaff /tmp/storm
+sudo chown -R `whoami` /tmp/storm
 mkdir /tmp/storm/logs/ -p
 mkdir /tmp/storm/conf/ -p
-cp ./emulab.yaml /tmp/storm/conf/storm.yaml
+cp $REPO_DIR/emulab.yaml /tmp/storm/conf/storm.yaml
 echo -e "    name: \"`hostname`\"" >> /tmp/storm/conf/storm.yaml
 
 # Start supervisord
 sudo pkill supervisord
 if [ `hostname` = $MASTER_NODE ];
 then
-    sudo cp supervisord_master.conf /etc/supervisor/supervisord.conf
+    sudo cp $REPO_DIR/supervisord_master.conf /etc/supervisor/supervisord.conf
     cd /proj/uiucnet/exp/storm/deps/zookeeper-3.4.5/
     sudo ./bin/zkServer.sh start
 else
-    sudo cp supervisord_slave.conf /etc/supervisor/supervisord.conf
+    sudo cp $REPO_DIR/supervisord_slave.conf /etc/supervisor/supervisord.conf
 fi
+
+sudo service supervisor restart
