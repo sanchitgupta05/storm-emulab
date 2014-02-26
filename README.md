724e483a4c52ea3f86c2ab6122b68c4018dab12d
Hilfi M. Alkaff <alkaff2@illinois.edu>
2014-02-25 21:02:49 -0600

    Remove some hardcoded variables.

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ storm-emulab
 
 The code in this repository serves to ease storm deployment into emulab.
 
-## Running the storm hosts
+## Installing the storm dependencies
+* ./init.sh
 
+## Running storm processes
+* ./storm-start
+
+## Killing storm processes
+* ./storm-kill
--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 #
 # This script setups storm's dependencies, needs to be run in all hosts.
 
+MASTER_NODE="hosts-1.storm.uiucnet.emulab.net"
+
 sudo apt-get update
 sudo apt-get install -y openjdk-7-jre openjdk-7-jdk build-essential pkg-config libtool zookeeper zookeeper-bin chkconfig supervisor maven2
 
@@ -14,12 +16,12 @@ sudo make install
 sudo chown -R halkaff /tmp/storm
 mkdir /tmp/storm/logs/ -p
 mkdir /tmp/storm/conf/ -p
-cp /proj/uiucnet/exp/storm/storm/conf/emulab.yaml /tmp/storm/conf/storm.yaml
+cp ./emulab.yaml /tmp/storm/conf/storm.yaml
 echo -e "    name: \"`hostname`\"" >> /tmp/storm/conf/storm.yaml
 
 # Start supervisord
 sudo pkill supervisord
-if [ `hostname` = "hosts-1.storm.uiucnet.emulab.net" ];
+if [ `hostname` = $MASTER_NODE ];
 then
     sudo cp supervisord_master.conf /etc/supervisor/supervisord.conf
     cd /proj/uiucnet/exp/storm/deps/zookeeper-3.4.5/
