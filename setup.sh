b625d7f79952c3f101d482263b7b26b3d7c9d10f
Sanchit Gupta <sgupta51@illinois.edu>
2015-04-30 17:29:42 -0500

    added deps download and setup

--- setup.sh
+++ setup.sh
@@ -2,5 +2,19 @@
 #
 # Setup script to bootstrap
 
+# download deps
+#create a directory for deps
+mkdir /proj/DCSQ/exp/storm/deps/
+
+#download zeromq and install it
 wget http://download.zeromq.org/zeromq-4.0.3.tar.gz
+tar -xfz zeromq-4.0.3.tar.gz
+cd /proj/DCSQ/exp/storm/deps/zeromq-4.0.3/
 ./autogen.sh && ./configure && make -j4
+sudo make install
+
+#Download JZMQ and install it
+
+cd /proj/DCSQ/exp/storm/deps/jzmq/
+sudo make install
+
