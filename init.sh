7878a8fbabc8ee9de45186e09ea8f802c03aeb3d
Sanchit Gupta <sgupta51@hosts-1.storm.dcsq.emulab.net>
2015-04-30 23:42:55 -0600

    differentiated configuration and installation

--- init.sh
+++ init.sh
@@ -2,43 +2,42 @@
 #
 # This script setups storm's dependencies, needs to be run in all hosts.
 
-MASTER_NODE="hosts-1.storm.DCSQ.emulab.net"
+MASTER_NODE="hosts-1.storm.dcsq.emulab.net"
 REPO_DIR="/proj/DCSQ/exp/storm/storm-emulab/"
 
 sudo apt-get update
 sudo apt-get install -y openjdk-7-jre openjdk-7-jdk build-essential pkg-config libtool zookeeper zookeeper-bin supervisor maven2 unzip
 
-cd /proj/DCSQ/exp/storm/deps
-
 #Download Storm and configure it
 # Storm lives in the tmp folded
 
-sudo chown -R `whoami` /tmp/storm
-cd /tmp
-wget http://apache.arvixe.com/storm/apache-storm-0.9.4/apache-storm-0.9.4.tar.gz 
-tar -xzf apache-storm-0.9.4.tar.gz
-sudo cp -R ./apache-storm-0.9.4 ./storm
-mkdir /tmp/storm/logs/ -p
+sudo chown -R `whoami` /usr/local/storm
 
-cp $REPO_DIR/emulab.yaml /tmp/storm/conf/storm.yaml
-echo -e "    name: \"`hostname`\"" >> /tmp/storm/conf/storm.yaml
+### Download Storm and create $STORM_HOME = /usr/local/storm
+
+#cd /tmp
+#wget http://apache.arvixe.com/storm/apache-storm-0.9.4/apache-storm-0.9.4.tar.gz 
+#cd /usr/local/
+#sudo tar -xzf /tmp/apache-storm-0.9.4.tar.gz
+#sudo chown -R sgupta51 apache-storm-0.9.4.tar.gz
+#sudo ln -s apache-storm-0.9.4 storm
+
+sudo mkdir /tmp/storm/logs/ -p
+
+sudo cp $REPO_DIR/emulab.yaml REPO_DIR/../storm/conf/storm.yaml
+#echo -e "    name: \"`hostname`\"" >> /tmp/storm/conf/storm.yaml
 
 # Start supervisord
 sudo pkill supervisord
+echo `hostname`
 if [ `hostname` = $MASTER_NODE ];
 then
     sudo cp $REPO_DIR/supervisord_master.conf /etc/supervisor/supervisord.conf
-    cd /proj/DCSQ/exp/storm/deps/zookeeper-3.4.5/
+    cd /proj/DCSQ/exp/storm/deps/zookeeper-3.4.6/
     sudo ./bin/zkServer.sh start
 else
     sudo cp $REPO_DIR/supervisord_slave.conf /etc/supervisor/supervisord.conf
 fi
 
 sudo service supervisor restart
-
-
-
-
-
-
 
